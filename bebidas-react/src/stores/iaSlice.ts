fbd57c5f53e2bead3998f1f36ceb3af16208616b
Jhafet19 <20223tn107@utez.edu.mx>
2025-08-04 21:28:43 -0600

    Add cambios

--- bebidas-react/src/stores/iaSlice.ts
+++ bebidas-react/src/stores/iaSlice.ts
@@ -2,8 +2,13 @@ import { StateCreator } from 'zustand'
 
 export type AISlice = {
     recipe: string
+    generateRecipe: (prompt: string) => Promise<void>
 }
 
-export const createAiSlice : StateCreator<AISlice> = () => ({
-    recipe: ''
+export const createAiSlice: StateCreator<AISlice> = () => ({
+    recipe: '',
+    generateRecipe: async (prompt) => {
+    console.log("🚀 ~ createAiSlice ~ prompt:", prompt)
+
+    }
 })
