b659a5616e0bcc00a997f058bd0f56232cc0af17
Jhafet19 <20223tn107@utez.edu.mx>
2025-11-18 12:43:12 -0600

    Comenzando la autentificacion

--- mern/uptask-backend/src/config/cors.ts
+++ mern/uptask-backend/src/config/cors.ts
@@ -1,8 +1,12 @@
-import { CorsOptions } from "cors";
+import {CorsOptions} from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const witheList = [process.env.FRONTEND_URL]
+
+        if (process.argv[2] === '--api') {
+            witheList.push(undefined)
+        }
         if (witheList.includes(origin)) {
             callback(null, true)
         } else {
--- mern/uptask-backend/src/server.ts
+++ mern/uptask-backend/src/server.ts
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db";
 import prpjectRputes from './routes/projectRoutes'
 import { corsConfig } from "./config/cors";
 import morgan from "morgan";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', prpjectRputes)
 
 export default app;
